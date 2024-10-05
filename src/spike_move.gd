cff6d2c6ccb3eb7dcedf4f4cf7ef11fb72c676f1
Josef Spjut <josef.spjut@gmail.com>
2024-10-05 17:22:34 -0400

    Add finn to player group and collision detect

--- src/spike_move.gd
+++ src/spike_move.gd
@@ -4,6 +4,12 @@ extends CharacterBody2D
 @export var speed = 100.0
 var direction = Vector2.RIGHT
 
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		get_node("/root/Main").call("_on_player_hit")
 
 func _physics_process(delta: float) -> void:
 	velocity = direction * speed

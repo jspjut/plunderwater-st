4df3b95a835cd2b1fff9b8bdffde381bafaf45d8
Josef Spjut <josef.spjut@gmail.com>
2024-10-05 10:57:22 -0400

    Added asset download script

---
+++ download_assets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FOLDER_ID=1W30kKZFvQiYGSA0h4WxJkmRKkaxUCwhQ
+
+# Install gdown if not already installed
+pip install gdown
+
+# Download the folder
+gdown https://drive.google.com/drive/folders/$FOLDER_ID -O .assets -- folder

1aaf8b4ba794ad15164e8a6ace880063d750fd56
Josef Spjut <josef.spjut@gmail.com>
2024-10-10 09:56:45 -0400

    Add animations for idle and swimming

--- src/captain_finn_move.gd
+++ src/captain_finn_move.gd
@@ -2,11 +2,22 @@ extends CharacterBody2D
 
 
 @export var speed = 400.0
+@onready var animated_sprite = $AnimatedSprite2D
 
+func _ready():
+	animated_sprite.play("idle")
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * speed
+	
+	if velocity.length() > 0:
+		animated_sprite.play("swim")
+		# Flip sprite based on movement direction
+		animated_sprite.flip_h = velocity.x < 0
+	else:
+		animated_sprite.play("idle")
+	
 	move_and_slide()
 
 	# Clamp position to level bounds
